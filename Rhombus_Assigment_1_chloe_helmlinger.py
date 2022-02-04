7aefdf16586bc30b9ae14e97466ddb811b80adb1
c-helmlinger <86612309+c-helmlinger@users.noreply.github.com>
2022-02-04 11:04:25 -0800

    Add files via upload

---
+++ Rhombus_Assigment_1_chloe_helmlinger.py
@@ -0,0 +1,25 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print ("         *   ")
+print ("        ***  ")
+print ("       ***** ")
+print ("      *******   ")
+print("     *********")
+print("    ***********")
+print("   *************")
+print("  ***************")
+print(" *****************")
+print("*******************")
+print(" *****************")
+print("  ***************")
+print("   *************")
+print("    ***********")
+print("     *********")
+print ("      *******   ")
+print ("       ***** ")
+print ("        ***  ")
+print ("         *   ")
